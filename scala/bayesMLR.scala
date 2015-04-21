cce344c857ae1cde036e784f7a66b94a1005ba55
luiarthur <luiarthur@yahoo.com>
2015-04-21 14:35:01 -0600

    yes

--- scala/bayesMLR.scala
+++ scala/bayesMLR.scala
@@ -1,1 +1,13 @@
 //https://github.com/scalanlp/breeze/wiki/Linear-Algebra-Cheat-Sheet
+
+
+import scala.collection.immutable.Vector.empty
+import scala.io.Source
+import java.io.File
+import scala.math._
+import util.Random
+import Gamma.Gamma._
+
+object bayesMLR{
+
+}
