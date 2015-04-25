5c8bd3ec4b36d7fed19234eb6fba80ac68857061
luiarthur <luiarthur@gmail.com>
2015-04-25 16:59:45 -0600

    yes

--- julia/bayesMLR.jl
+++ julia/bayesMLR.jl
@@ -5,11 +5,13 @@
 #ta = 1:size(X,2); (ta .!= 3) & (ta .!= 5)
 #blas_set_num_thread(8)
 
+println("Loading packages...")
 using DataFrames
 using Distributions
+println("Finished loading packages...")
 
 const dat = readdlm("../data/dat.txt")
-const n,k = size(dat); k = k-1
+n,k = size(dat); k = k-1
 
 const y =dat[:,1]
 const X = dat[:,2:end]
@@ -26,41 +28,43 @@ s2=10
 function ll(be,sig2) c = y-X*be; c'c/(-2sig2) - n*log(sig2)/2 end
 function lpb(be) -be'XXi*be/2s2 end
 function lps(sig2) +(a-1)*log(s2)-s2/b end
-function mvrnorm(M,S,n=size(S,2)) M+chol(S)'randn(n) end
+function mvrnorm(M,S) M+chol(S)'randn(k) end
 
 csb = 4*XXi
 css = 1
 accb = 0
 accs = 0
 
-bb = zeros(B,k)
+bb = zeros(k,B)
 ss = ones(B)
 
-bcur = bb[1,:]'
-scur = ss[i]
+bcur = bb[:,1]
+scur = ss[1]
 r = log(rand(B*2))
 
-for i in 1:B
-  bb[i,] = bcur
+@time for i in 1:B
+  bb[:,i] = bcur
   ss[i]  = scur
 
   # Update β̂: 
   candb = mvrnorm(bcur,csb)
   q = ll(candb,scur)+lpb(candb) -ll(bcur,scur)-lpb(bcur)
-  if q>r[i] 
+  if q[1]>r[i] 
     bcur = candb
     accb += 1
   end
 
   # Update ŝ²:
-  cands = rand(Normal(scur,sqrt(css)))
+  #cands = rand(Normal(scur,sqrt(css)))
+  cands = randn()*sqrt(css)+scur
   if cands>0
-    q = ll(bcur,cands)+lps(cands) -ll(bcur,scur)-lpb(scur)
-    if q>r[i+B]
+    q = ll(bcur,cands)+lps(cands) -ll(bcur,scur)-lps(scur)
+    if q[1]>r[i+B]
       scur = cands
       accs += 1
     end
   end
 
-  #print("\r",round(100i/B),"%")
+  print("\r",round(100i/B),"%")
 end
+
