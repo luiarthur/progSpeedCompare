3c90bcba0cfc63a4b244ea69a60e85f5fd2595e7
luiarthur <luiarthur@gmail.com>
2015-04-25 17:57:18 -0600

    yes

--- julia/bayesMLR.jl
+++ julia/bayesMLR.jl
@@ -11,7 +11,8 @@ using Distributions
 println("Finished loading packages...")
 
 const dat = readdlm("../data/dat.txt")
-n,k = size(dat); k = k-1
+n,k1 = size(dat)
+const k = k1-1
 
 const y =dat[:,1]
 const X = dat[:,2:end]
@@ -24,30 +25,37 @@ a = 1
 b = 1
 B = 100000
 s2=10
-
-function ll(be,sig2) c = y-X*be; c'c/(-2sig2) - n*log(sig2)/2 end
-function lpb(be) -be'XXi*be/2s2 end
-function lps(sig2) +(a-1)*log(s2)-s2/b end
-function mvrnorm(M,S) M+chol(S)'randn(k) end
-
 csb = 4*XXi
 css = 1
 accb = 0
 accs = 0
+S = chol(csb)'
 
-bb = zeros(k,B)
-ss = ones(B)
+function ll(be::Array{Float64,1},sig2::Float64) 
+  c = y-X*be
+  c'c/(-2sig2)-n*log(sig2)/2 
+end
+
+lpb(be::Array{Float64,1}) = -be'XXi*be/2s2
+lps(sig2::Float64) = (a-1)*log(s2)-s2/b 
+mvrnorm(M::Array{Float64,1}) = M+S*randn(k)
+
+
+bb = Array(Float64,(k,B))
+ss = Array(Float64,B)
 
 bcur = bb[:,1]
 scur = ss[1]
 r = log(rand(B*2))
-
+rnorm = randn(B)
+println("Starting Metropolis...")
+#@time @fastmath for i in 1:B
 @time for i in 1:B
-  bb[:,i] = bcur
-  ss[i]  = scur
+  @inbounds bb[:,i] = bcur
+  @inbounds ss[i]  = scur
 
   # Update β̂: 
-  candb = mvrnorm(bcur,csb)
+  candb = mvrnorm(bcur)
   q = ll(candb,scur)+lpb(candb) -ll(bcur,scur)-lpb(bcur)
   if q[1]>r[i] 
     bcur = candb
@@ -56,7 +64,7 @@ r = log(rand(B*2))
 
   # Update ŝ²:
   #cands = rand(Normal(scur,sqrt(css)))
-  cands = randn()*sqrt(css)+scur
+  cands = rnorm[i]*sqrt(css)+scur
   if cands>0
     q = ll(bcur,cands)+lps(cands) -ll(bcur,scur)-lps(scur)
     if q[1]>r[i+B]
@@ -65,6 +73,12 @@ r = log(rand(B*2))
     end
   end
 
-  print("\r",round(100i/B),"%")
+  prog = round(100.0*i/B)
+  #print("\r",prog,"%") #this doubles the time
 end
 
+println(mean(bb,2))
+println(mean(ss))
+
+println(accb/B)
+println(accs/B)
