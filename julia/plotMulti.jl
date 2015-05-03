d68c0b5a4c1ec2610afe2242229b372588ea0336
luiarthur <luiarthur@gmail.com>
2015-05-02 19:06:01 -0600

    yes

--- julia/plotMulti.jl
+++ julia/plotMulti.jl
@@ -1,15 +1,12 @@
 using Gadfly, DataFrames
 
-y1(n) = 2n
-y2(n) = n+2
-y3(n) = sin(n)
-
 N=10
-x = repeat([1:N],outer=[3])
-ys = vcat([y1(n) for n=1:N],
-          [y2(n) for n=1:N], 
-          [y3(n) for n=1:N])
+x = [1:N]
+y = [x,x*2,x*3]
+xs = repeat([1:N],outer=[3])
 z = repeat(["1","2","3"], inner=[N])
 
-df = DataFrame(x=x,y=y,z=z)
+df = DataFrame(x=xs,y=y,z=z)
 plot(df, x="x", y="y",color="z", Geom.point, Geom.line)
+
+
