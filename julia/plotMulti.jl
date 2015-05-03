e44c6dbe2fba93fb095079d57097561d0b5f48af
luiarthur <luiarthur@gmail.com>
2015-05-02 19:01:42 -0600

    yes

--- julia/plotMulti.jl
+++ julia/plotMulti.jl
@@ -6,10 +6,10 @@ y3(n) = sin(n)
 
 N=10
 x = repeat([1:N],outer=[3])
-y = vcat([y1(n) for n=1:N],
-         [y2(n) for n=1:N], 
-         [y3(n) for n=1:N])
-col = repeat(["1","2","3"], inner=[N])
+ys = vcat([y1(n) for n=1:N],
+          [y2(n) for n=1:N], 
+          [y3(n) for n=1:N])
+z = repeat(["1","2","3"], inner=[N])
 
-df = DataFrames.DataFrame(d1=x,d2=y,d3=col)
-plot(df, x="d1", y="d2", color="d3", Geom.point, Geom.line)
+df = DataFrame(x=x,y=y,z=z)
+plot(df, x="x", y="y",color="z", Geom.point, Geom.line)
