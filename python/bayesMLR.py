0889226b64c788c31a8b3a62a8f03875a6a34955
luiarthur <luiarthur@gmail.com>
2015-10-29 22:11:13 -0700

    yes

--- python/bayesMLR.py
+++ python/bayesMLR.py
@@ -1,2 +1,11 @@
-f = open ( '../data/dat.txt' , 'r')
-dat = [ map(float,line.split(' ')) for line in f if line.strip() != "" ]
+import numpy as np
+dat = np.genfromtxt('../data/dat.txt',delimiter=' ')
+
+y = dat[:,0]
+X = dat[:,1:11]
+
+k = X.shape[1]
+n = y.shape[0]
+
+Xt = X.transpose()
+XXi = 
