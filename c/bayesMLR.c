e10e89e9d91527c2c45eaa063523457e270c0765
Arthur Lui <luiarthur@gmail.com>
2015-12-14 16:16:08 -0800

    yes

--- c/bayesMLR.c
+++ c/bayesMLR.c
@@ -7,6 +7,7 @@
 #include <gsl/gsl_cdf.h>       // GNU Scientific Library
 #include <gsl/gsl_randist.h>   // GNU Scientific Library
 #include <gsl/gsl_linalg.h>    // GNU Scientific Library
+#include <gsl/gsl_matrix.h>    // GNU Scientific Library
 
 //#include "/data/arthurll/arthurPackages/c/vector/vector.h"
 
@@ -18,11 +19,39 @@ double runif() {
   return (double) rand() / (double) RAND_MAX;
 }
 
-
-
-int main(int argc, char* argv[]) {
-
-
+double gibbs(int* n, int* k, gsl_matrix* mle) {
 }
 
+//void readmatrix(int n, int k, gsl_matrix* m, char* filename) {
+//  FILE* file=fopen(filename, "r");
+//  for (int i = 0; i < n; i++)
+//    for (int j = 0; j < k; j++)
+//      gsl_matrix_set (m, i, j, 0.23 + 100*i + j);
+//  
+//  //for (int i = 0; i < n; i++)  /* OUT OF RANGE ERROR */
+//  //  for (int j = 0; j < k; j++)
+//  //    printf ("m(%d,%d) = %g\n", i, j, 
+//  //            gsl_matrix_get (m, i, j));
+//}
 
+int main(int argc, char* argv[]) {
+  int n = 4;
+  int k = 2;
+
+  // initialize m
+  gsl_matrix* m = gsl_matrix_alloc(n,k);
+
+  // read data
+  FILE* f = fopen("test/dat.txt", "r");
+  gsl_matrix_fscanf(f, m);
+  fclose(f);
+
+  // write data
+  FILE* o = fopen("fout.txt", "w");
+  gsl_matrix_fprintf(o, m,"%f\t");
+  fclose(o);
+
+
+  // free memory
+  gsl_matrix_free(m);
+}
