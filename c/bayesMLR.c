1109c60b6dcaf4cf763637eac42c681a8167fe94
Arthur Lui <luiarthur@gmail.com>
2015-12-17 13:03:35 -0800

    yes

--- c/bayesMLR.c
+++ c/bayesMLR.c
@@ -1,14 +1,47 @@
 #include "my_gsl.h" // print & read matrices / other functions
 
-//lpb(be::Array{Float64,1}) = -be'XXi*be/2s2
 double lpb(gsl_vector* b, gsl_matrix* XXi, double s2) {
+  // b' XXi b / -(2*s2)
+  int n = b->size;
+  double out = 0;
+  double rowSum;
+
+  for (int i=0; i<n; i++) {
+    rowSum = 0;
+    for (int j=0; j<n; j++) {
+      rowSum += gsl_vector_get(b,j) * gsl_matrix_get(XXi,i,j);
+    }
+    out += rowSum * gsl_vector_get(b,i);
+  }
+
+  out = out / -(2*s2);
+
+  return out;
 }
 
-//lps(sig2::Float64) = (a-1)*log(s2)-s2/b 
 double lps(double s2, double a, double b) {
+  return (a-1) * log(s2) - s2/b;
 }
 
-double ll(gsl_vector* be, double sig2, gsl_vector* y, gsl_matrix* X) {
+double ll(gsl_vector* b, double s2, gsl_vector* y, gsl_matrix* X) {
+  // c = y-X*be
+  // c'c/(-2sig2)-n*log(sig2)/2 
+  double out = 0;
+  double yi;
+  double Xbi;
+  int n = b->size;
+
+  for (int i=0; i<n; i++) {
+    Xbi = 0;
+    for (int j=0; j<n; j++) {
+      Xbi += gsl_matrix_get(X,i,j) * gsl_vector_get(b,j);
+    }
+    yi = gsl_vector_get(y,i);
+    out += pow(yi - Xbi,2);
+  }
+  
+  out = out / (-2*s2) - n * log(s2) / 2;
+  return out;
 }
 
 int main(int argc, char* argv[]) {
@@ -16,12 +49,46 @@ int main(int argc, char* argv[]) {
   gsl_rng* r = gsl_rng_alloc(gsl_rng_taus);
   char* filename = "../data/dat.txt";
   int n = countFileRows(filename);
-  int k = countFileCols(filename,' ');
-  gsl_matrix* M = gsl_matrix_alloc(n,k);
+  int k1 = countFileCols(filename,' ');
+  int k = k1 - 1;
 
-  printf("%d%s%d\n",n,"x",k);
+  gsl_matrix* M = gsl_matrix_alloc(n,k1);
+  gsl_matrix* X = gsl_matrix_alloc(n,k);
+  gsl_vector* y = gsl_vector_alloc(n);
+  gsl_matrix* csb = gsl_matrix_alloc(k,k);
+  gsl_matrix* cholS = gsl_matrix_alloc(k,k);
+
+  read_csv(filename, M);
+  mat_sub(M,0,n-1,1,k,X);
+  gsl_matrix_get_col(y,M,0);
+
+  xxi_m(X,csb);
+  gsl_matrix_scale(csb,4);
+
+  int B = 100;
+  int acc_b = 0;
+  int acc_s = 0;
+  gsl_matrix* bb = gsl_matrix_alloc(B,k);
+  gsl_vector* ss = gsl_vector_alloc(B);
+  gsl_vector* candb = gsl_vector_alloc(k);
+
+  for (int b=1; b<B; b++) {
+    // update beta
+    //mvrnorm(m,chols,r,v)
+
+    // update s2
+
+  }
+
 
   // Free Memory:
+  gsl_matrix_free(cholS);
+  gsl_vector_free(candb);
+  gsl_vector_free(ss);
+  gsl_matrix_free(bb);
+  gsl_matrix_free(csb);
+  gsl_vector_free(y);
+  gsl_matrix_free(X);
   gsl_matrix_free(M);
   gsl_rng_free(r);
 
--- c/my_gsl.h
+++ c/my_gsl.h
@@ -61,6 +61,17 @@ void read_csv(char* filename, gsl_matrix* m) {
 }
 
 // Printing Matrix Info ////////////////////////////
+void print_vector(gsl_matrix* v, char* filename) {
+  FILE* f;
+  if (filename == "") {
+    f = stdout;
+    printf("\n");
+  } else {
+    f = fopen(filename, "w");
+  }
+
+  gsl_vector_fprintf(f,v,"%g");
+}
 
 void print_matrix(gsl_matrix* m, char* filename) {
   // print matrix to filename or stdout if filename == ""
@@ -91,6 +102,12 @@ void print_matrix(gsl_matrix* m, char* filename) {
 void print_matrix_dims(gsl_matrix* m) {
   // print dimensions of matrix m
   printf("%d%s%d\n",m->size1,"x",m->size2);
+}
+
+// Access Matrix Elements: //////////////////////////////////
+void mat_sub(gsl_matrix* X, size_t r1, size_t r2, size_t c1, size_t c2, gsl_matrix* X_new) {
+  gsl_matrix_view x = gsl_matrix_submatrix(X,r1,c1,r2-r1+1,c2-c1+1);
+  gsl_matrix_memcpy(X_new,&x.matrix);
 }
 
 // Random Number Functions: /////////////////////////////////
@@ -156,9 +173,166 @@ double vv_prod(gsl_vector* x, gsl_vector* y) {
   int n = x->size;
 
   for (int i=0; i<n; i++) {
-    out += gsl_vector_get(x,i)*gsl_vector_get(y,i);
+    out += gsl_vector_get(x,i) * gsl_vector_get(y,i);
   }
 
   return out;
 }
 
+
+void mat_inv(gsl_matrix * X, gsl_matrix * invX) {
+// invert the matrix X
+    int signum;
+    gsl_matrix * LU = gsl_matrix_calloc(X->size1,X->size2);
+    gsl_permutation * p = gsl_permutation_calloc(X->size1);
+    gsl_matrix_memcpy(LU,X);
+
+    gsl_linalg_LU_decomp (LU,p,&signum);
+    gsl_linalg_LU_invert(LU,p,invX);
+
+    gsl_matrix_free(LU);
+    gsl_permutation_free(p);
+}
+
+void xxi_m(gsl_matrix* X, gsl_matrix* xxi) {
+  int n = X->size1;
+  int k = X->size2;
+  double dot;
+  gsl_matrix* xx = gsl_matrix_alloc(k,k);
+
+  for (int i=0; i<k; i++) {
+    for (int j=0; j<=i; j++) {
+      dot = 0;
+      for (int z=0; z<n; z++) {
+        dot += gsl_matrix_get(X,z,i) * gsl_matrix_get(X,z,j);
+      }
+      gsl_matrix_set(xx,j,i,dot);
+      gsl_matrix_set(xx,i,j,dot);
+    }
+  }
+
+  mat_inv(xx,xxi);
+  gsl_matrix_free(xx);
+}
+
+
+int my_gsl_linalg_cholesky_decomp (gsl_matrix * A)
+{
+  const size_t M = A->size1;
+  const size_t N = A->size2;
+
+  if (M != N)
+    {
+      GSL_ERROR("cholesky decomposition requires square matrix", GSL_ENOTSQR);
+    }
+  else
+    {
+      size_t i,j,k;
+      int status = 0;
+
+      /* Do the first 2 rows explicitly.  It is simple, and faster.  And
+       * one can return if the matrix has only 1 or 2 rows.  
+       */
+
+      double A_00 = gsl_matrix_get (A, 0, 0);
+      
+      double L_00 = sqrt(A_00);
+      
+      if (A_00 <= 0)
+        {
+          status = GSL_EDOM ;
+        }
+
+      gsl_matrix_set (A, 0, 0, L_00);
+  
+      if (M > 1)
+        {
+          double A_10 = gsl_matrix_get (A, 1, 0);
+          double A_11 = gsl_matrix_get (A, 1, 1);
+          
+          double L_10 = A_10 / L_00;
+          double diag = A_11 - L_10 * L_10;
+          double L_11 = sqrt(diag);
+          
+          if (diag <= 0)
+            {
+              status = GSL_EDOM;
+            }
+
+          gsl_matrix_set (A, 1, 0, L_10);        
+          gsl_matrix_set (A, 1, 1, L_11);
+        }
+      
+      for (k = 2; k < M; k++)
+        {
+          double A_kk = gsl_matrix_get (A, k, k);
+          
+          for (i = 0; i < k; i++)
+            {
+              double sum = 0;
+
+              double A_ki = gsl_matrix_get (A, k, i);
+              double A_ii = gsl_matrix_get (A, i, i);
+
+              gsl_vector_view ci = gsl_matrix_row (A, i);
+              gsl_vector_view ck = gsl_matrix_row (A, k);
+
+              if (i > 0) {
+                gsl_vector_view di = gsl_vector_subvector(&ci.vector, 0, i);
+                gsl_vector_view dk = gsl_vector_subvector(&ck.vector, 0, i);
+                
+                gsl_blas_ddot (&di.vector, &dk.vector, &sum);
+              }
+
+              A_ki = (A_ki - sum) / A_ii;
+              gsl_matrix_set (A, k, i, A_ki);
+            } 
+
+          {
+            gsl_vector_view ck = gsl_matrix_row (A, k);
+            gsl_vector_view dk = gsl_vector_subvector (&ck.vector, 0, k);
+            
+            double sum = gsl_blas_dnrm2 (&dk.vector);
+            double diag = A_kk - sum * sum;
+
+            double L_kk = sqrt(diag);
+            
+            if (diag <= 0)
+              {
+                status = GSL_EDOM;
+              }
+            
+            gsl_matrix_set (A, k, k, L_kk);
+          }
+        }
+
+      /* Now copy the transposed lower triangle to the upper triangle,
+       * the diagonal is common.  
+       */
+      
+      for (i = 1; i < M; i++)
+        {
+          for (j = 0; j < i; j++)
+            {
+              double A_ij = 0;//gsl_matrix_get (A, i, j);
+              gsl_matrix_set (A, j, i, A_ij);
+            }
+        } 
+      
+      if (status == GSL_EDOM)
+        {
+          GSL_ERROR ("matrix must be positive definite", GSL_EDOM);
+        }
+      
+      return GSL_SUCCESS;
+    }
+}
+
+void chol(gsl_matrix* X, gsl_matrix* cholS) {
+  int n = X->size1;
+  gsl_matrix_memcpy(cholS,X);
+  my_gsl_linalg_cholesky_decomp(cholS);
+
+  // lower tri stored in cholS.
+  // So: mvrnorm <- function(M,S,n=nrow(S)) M + chol(S) %*% rnorm(n)
+}
